b2ab0c8424d5eabfe79527cb1f12a1da0122492e
timofti <vincent.timofti@telecom-paristech.fr>
2015-04-02 11:39:15 +0200

    project files

---
+++ src/infsi351/Restauration/FragmentServeur.java
@@ -0,0 +1,31 @@
+package infsi351.Restauration;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentServeur extends Fragment {
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+	public static final String ARG_SECTION_NUMBER = "section_number";
+
+	public FragmentServeur() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Create a new TextView and set its text to the fragment's section
+		// number argument value.
+		TextView textView = new TextView(getActivity());
+		textView.setGravity(Gravity.CENTER);
+		textView.setText("Serveur");
+		return textView;
+	}
+}
---
+++ src/infsi351/Restauration/MainActivity.java
@@ -0,0 +1,107 @@
+package infsi351.Restauration;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends FragmentActivity implements
+		ActionBar.TabListener {
+
+	/**
+	 * The serialization (saved instance state) Bundle key representing the
+	 * current tab position.
+	 */
+	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Set up the action bar to show tabs.
+		final ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// For each of the sections in the app, add a tab to the action bar.
+		actionBar.addTab(actionBar.newTab().setText(R.string.title_section1)
+				.setTabListener(this));
+		actionBar.addTab(actionBar.newTab().setText(R.string.title_section2)
+				.setTabListener(this));
+		actionBar.addTab(actionBar.newTab().setText(R.string.title_section3)
+				.setTabListener(this));
+		actionBar.addTab(actionBar.newTab().setText(R.string.title_section4)
+				.setTabListener(this));
+		actionBar.addTab(actionBar.newTab().setText(R.string.title_section5)
+				.setTabListener(this));
+	}
+
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState) {
+		// Restore the previously serialized current tab position.
+		if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
+			getActionBar().setSelectedNavigationItem(
+					savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
+		}
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		// Serialize the current tab position.
+		outState.putInt(STATE_SELECTED_NAVIGATION_ITEM, getActionBar()
+				.getSelectedNavigationIndex());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public void onTabSelected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+		// When the given tab is selected, show the tab contents in the
+		// container view.
+		
+		int tab_num = tab.getPosition();
+			
+		Fragment frag;
+
+		if (tab_num == 0)
+			frag = new FragmentAccueil();
+		else if (tab_num == 1)
+			frag = new FragmentComposition();
+		else if (tab_num == 2)
+			frag = new FragmentBoisson();
+		else if (tab_num == 3)
+			frag = new FragmentServeur();
+		else
+			frag = new FragmentPanier();
+		
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.container, frag).commit();
+	}
+
+	@Override
+	public void onTabUnselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+	}
+
+	@Override
+	public void onTabReselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+	}
+
+
+}
