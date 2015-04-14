e543bbdefe927ddbdb545e2e311a4560409d2400
Vincent Timofti <vincent.timofti@gmail.com>
2015-04-14 16:33:18 +0200

    integration dynamique des ingredients pour la composotion

--- src/infsi351/Restauration/FragmentComposition2.java
+++ src/infsi351/Restauration/FragmentComposition2.java
@@ -1,11 +1,19 @@
 package infsi351.Restauration;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.GridLayout;
+import android.widget.ToggleButton;
+
+import java.util.Map;
+import java.util.List;
 
 public class FragmentComposition2 extends Fragment {
 	/**
@@ -23,10 +31,39 @@ public class FragmentComposition2 extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		View view = inflater.inflate(R.layout.composition_layout2, container, false);
-//		TextView textView = (TextView) view.findViewById(R.id.textView1);
-//		textView.setText(base);
+		Resources res = getResources();
+		String packageName = getActivity().getPackageName();
 		
+		View view = inflater.inflate(R.layout.composition_layout2, container, false);
+		GridLayout gridLayout = (GridLayout) view.findViewById(R.id.gridLayout);
+		float density = res.getDisplayMetrics().density;
+		
+		for (Map.Entry<String, List<String>> entry : Pizza.ingredients.entrySet())
+		{
+			View viewIngredient = inflater.inflate(R.layout.liste_ingredients, (ViewGroup) view, false);
+			TextView titre = (TextView) viewIngredient.findViewById(R.id.titreIngredient);
+			titre.setText(res.getString(res.getIdentifier(entry.getKey(), "string", packageName)));
+			
+			GridLayout ingredientGrid = (GridLayout) viewIngredient.findViewById(R.id.gridIngredient);
+			
+			for(String ingredient : entry.getValue()) {
+				ToggleButton button = new ToggleButton(getActivity());
+				
+				String ingredientString = res.getString(res.getIdentifier(ingredient, "string", packageName));
+				Log.d("app", ingredientString);
+				button.setText(ingredientString);
+				button.setTextOff(ingredientString);
+				button.setTextOn(ingredientString);
+				button.setWidth((int)(100 * density));
+				ingredientGrid.addView(button);
+				
+				
+			}
+			
+			gridLayout.addView(viewIngredient);
+		    Log.d("app",entry.getKey() + "/" + entry.getValue());
+		}
+
 		return view;
 	}
 	
---
+++ src/infsi351/Restauration/FragmentComposition3.java
@@ -0,0 +1,34 @@
+package infsi351.Restauration;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentComposition3 extends Fragment {
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+	public static final String ARG_SECTION_NUMBER = "section_number";
+	private String base;
+
+	public FragmentComposition3(String base) {
+		this.base = base;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.composition_layout2, container, false);
+//		TextView textView = (TextView) view.findViewById(R.id.textView1);
+//		textView.setText(base);
+		
+		return view;
+	}
+	
+
+}
