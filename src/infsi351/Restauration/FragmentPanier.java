294a9320fcc069ff75c8e011891abecaecd812d4
antkahn <ant.kahn@gmail.com>
2015-04-16 11:57:54 +0200

    quelques changements du panier

--- src/infsi351/Restauration/FragmentPanier.java
+++ src/infsi351/Restauration/FragmentPanier.java
@@ -1,12 +1,15 @@
 package infsi351.Restauration;
 
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -25,22 +28,42 @@ public class FragmentPanier extends Fragment {
 			Bundle savedInstanceState) {
 		// Create a new TextView and set its text to the fragment's section
 		// number argument value.
+		
+		MainActivity main = (MainActivity)getActivity();
+		
 		View view = inflater.inflate(R.layout.panier, container, false);
 		View content_cart = view.findViewById(R.id.cart_content);
 		View content_drinks = view.findViewById(R.id.drink_content);
-		for(int i =0; i < 2; i ++){
-			View item_cart = inflater.inflate(R.layout.cart_item, (ViewGroup) content_cart, false);
-			((TextView)item_cart.findViewById(R.id.cart_text_item)).setText("Pizza numero " + i);
-			((TextView)item_cart.findViewById(R.id.cart_price_item)).setText((i+1)*10 + "ï¿½");
-			((LinearLayout)content_cart).addView(item_cart);
-			
-			View item_drink = inflater.inflate(R.layout.drink_item, (ViewGroup) content_drinks, false);
-			((TextView)item_drink.findViewById(R.id.cart_text_item)).setText("Boisson numero " + i);
-			((TextView)item_drink.findViewById(R.id.cart_price_item)).setText((i+1)*10 + "ï¿½");
+		
+		List<Boisson> drink_list = main.commande.get_liste_boissons();
+		List<Pizza> pizza_list = main.commande.get_liste_pizzas();
+		
+		double total = 0;
+		
+		if(drink_list.size() > 0){
+			for(int i =0; i < drink_list.size(); i ++){
+				View item_drink = inflater.inflate(R.layout.drink_item, (ViewGroup) content_drinks, false);
+				((TextView)item_drink.findViewById(R.id.cart_text_item)).setText(drink_list.get(i).getNom());
+				((TextView)item_drink.findViewById(R.id.cart_price_item)).setText(drink_list.get(i).getPrix() + "€");
+				((LinearLayout)content_drinks).addView(item_drink);
+				total += drink_list.get(i).getPrix();
+			}
+		}
+		else {
+			View item_drink = inflater.inflate(R.layout.empty_cart, (ViewGroup) content_drinks, false);
+			((TextView)item_drink.findViewById(R.id.cart_text_item)).setText("Pas de boisson ? :(");
 			((LinearLayout)content_drinks).addView(item_drink);
 		}
+		for(int i =0; i < pizza_list.size(); i++){
+			View item_cart = inflater.inflate(R.layout.cart_item, (ViewGroup) content_cart, false);
+			((TextView)item_cart.findViewById(R.id.cart_text_item)).setText("Pizza base " + pizza_list.get(i).getBase());
+			((TextView)item_cart.findViewById(R.id.item_description)).setText(pizza_list.get(i).getIngredient().toString());
+			((TextView)item_cart.findViewById(R.id.cart_price_item)).setText((i+1)*10 + "€");
+			((LinearLayout)content_cart).addView(item_cart);
+			total += (i+1)*10;
+		}
 		
-		((TextView)view.findViewById(R.id.total_cart)).setText(60 + "ï¿½");
+		((TextView)view.findViewById(R.id.total_cart)).setText(total + " €");
 		
 		return view;
 	}
