e56c504b0a5af5b447257681a1d14789ab89435a
Giulia Zago <giuliazago@MacBook-Pro-di-Giulia-2.local>
2015-04-18 10:24:20 +0200

    samedi matin

---
+++ src/infsi351/Restauration/FragmentAddition.java
@@ -0,0 +1,31 @@
+package infsi351.Restauration;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentAddition extends Fragment {
+	
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+	public static final String ARG_SECTION_NUMBER = "section_number";
+	
+	public FragmentAddition(){
+		
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// Create a new TextView and set its text to the fragment's section
+		// number argument value.
+		
+		View view = inflater.inflate(R.layout.addition_fragment, container, false);
+		return view;
+	}
+	
+
+}
